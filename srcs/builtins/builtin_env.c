1268810dacdba7ffd07d9ca9a67681ea0378dc26
Francois THIERREE <frthierr@e1r9p6.42.fr>
2020-08-14 12:59:39 +0200

    fixed env and export on empty variables

--- srcs/builtins/builtin_env.c
+++ srcs/builtins/builtin_env.c
@@ -3,14 +3,24 @@
 /*                                                        :::      ::::::::   */
 /*   builtin_env.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: qfeuilla <qfeuilla@student.42.fr>          +#+  +:+       +#+        */
+/*   By: frthierr <frthierr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/03 14:06:01 by frthierr          #+#    #+#             */
-/*   Updated: 2020/07/21 13:33:41 by qfeuilla         ###   ########.fr       */
+/*   Updated: 2020/08/14 12:57:17 by frthierr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+int		is_empty(char *envvar)
+{
+	char	*end_of_key;
+
+	end_of_key = ft_strchr((const char*)envvar, '=');
+	if (!end_of_key)
+		return (1);
+	return (0);
+}
 
 int		builtin_env(void)
 {
@@ -18,6 +28,10 @@ int		builtin_env(void)
 
 	i = 0;
 	while (g_env[i])
-		ft_putendl_fd(g_env[i++], STDOUT_FILENO);
+	{
+		if (!is_empty(g_env[i]))
+			ft_putendl_fd(g_env[i], STDOUT_FILENO);
+		i++;
+	}
 	return (0);
 }
