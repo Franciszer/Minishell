668699ef9b538c00bf22be5bccf61571dc4e63a9
user42 <ftthierree@gmail.com>
2020-08-22 13:46:57 +0200

    export fixed

--- srcs/builtins/builtin_export3.c
+++ srcs/builtins/builtin_export3.c
@@ -12,42 +12,87 @@
 
 #include "minishell.h"
 
+char		*remove_plus_sign(char *arg)
+{
+	char	*new_arg;
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = 0;
+	if (!(new_arg = malloc(sizeof(char) * (ft_strlen(arg) + 1))))
+		return (NULL);
+	while (arg[i])
+	{
+		if (arg[i] != '+')
+			new_arg[j++] = arg[i++];
+		else
+			i++;
+	}
+	return (new_arg);
+}
+
+
+t_list		*replance_envvar_init_vals(int *replaced)
+{
+	t_list	*env_list;
+
+	if (!(env_list = ft_argv_to_list(g_env)))
+		return (NULL);
+	*replaced = 0;
+	if (g_env_modified)
+		free_argv(g_env, INT_MAX); 
+	g_env_modified = 1;
+	return (env_list);
+}
+
+int			replace_envvar_util(t_list *nav, char *arg)
+{
+	char	*current_env;
+	size_t	len;
+
+	current_env = (char*)nav->content;
+	len = ft_strlen(current_env);
+	if (current_env[len - 1] != '=' &&\
+		!ft_strncmp(current_env, arg, ft_strlen(current_env)))
+	{
+		free(nav->content);
+		if (!(nav->content = ft_strdup(arg)))
+			return (-1);
+		return (1);
+	}
+	return (0);
+}
+
+
 int			replace_envvar(char *arg)
 {
 	t_list	*env_list;
 	t_list	*nav;
 	char	*current_env;
-	size_t	len;
 	int		replaced;
+	char	*new_arg;
 
 	if (!ft_strchr(arg, '=') && (current_env = get_env(arg)))
 	{
 		free(current_env);
 		return (1);
 	}
-	if (!(env_list = ft_argv_to_list(g_env)))
+	if (!(new_arg = remove_plus_sign(arg)))
 		return (0);
-	replaced = 0;
-	if (g_env_modified)
-		free_argv(g_env, INT_MAX); 
-	g_env_modified = 1;
+	if (!(env_list = replance_envvar_init_vals(&replaced)))
+		return (0);
 	nav = env_list;
 	while (nav && !replaced)
 	{
-		current_env = (char*)nav->content;
-		len = ft_strlen(current_env);
-		if (current_env[len - 1] != '=' && !ft_strncmp(current_env, arg, ft_strlen(current_env)))
-		{
-			free(nav->content);
-			if (!(nav->content = ft_strdup(arg)))
-				return (0);
-			replaced = 1;
-		}
+		if ((replaced = replace_envvar_util(nav, new_arg)) == -1)
+			return (0);
 		nav = nav->next;
 	}
 	if (!(g_env = list_to_argv(env_list)))
 		return (0);
 	ft_lstclear(&env_list, free);
+	free(new_arg);
 	if (replaced)
 		return (1);
 	return (0);
